53139f8eedcbd72dcf6d4fae3c6d5eac7f4f0128
toriroyal <tori.royal@sbcglobal.net>
2019-11-08 16:48:15 -0700

    Added Dominique's suggestions to difference.m

--- get_video_frames.m
+++ get_video_frames.m
@@ -11,8 +11,7 @@ function [num_frames, vid_frames] = get_video_frames(vid_file)
     i = 1; % variable to iterate through vid_frames
 
     while hasFrame(vreader) % while MATLAB can find videoframes, put each frame into vid_frames
-        new_frame = readFrame(vreader);
-        vid_frames{i} = image_fit(new_frame); % image_fit will downsample the image if neccesary
+        vid_frames{i} = image_fit(readFrame(vreader)); % image_fit will downsample the image if neccesary
         i = i + 1;
     end
 end
