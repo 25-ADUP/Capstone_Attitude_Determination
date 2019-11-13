cbbc3d261923601fd1e9fc4c9e86267f8238cfa0
toriroyal <tori.royal@sbcglobal.net>
2019-11-13 00:43:27 -0700

    Edited videowriting details.

--- postprocess.m
+++ postprocess.m
@@ -11,6 +11,8 @@ tic
 generate_heatmap(num_lib_frames, num_frames, lib_contour, vid_contour)
 
 output_vid = VideoWriter(config.OUTPUT_FILE); % Attidude Determination video for Cone1 Animation
+output_vid.FrameRate = frame_rate;
+output_vid.Quality = 100;
 open(output_vid);
 
 for i = 1:num_frames
