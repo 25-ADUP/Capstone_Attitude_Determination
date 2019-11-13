2753af91cdcda6d33ec0f1bee3f309f71c5e44aa
Steven <kaiser.steven16@gmail.com>
2019-11-13 00:20:14 -0700

    postprocess with video reconstruct

--- postprocess.m
+++ postprocess.m
@@ -8,16 +8,14 @@ tic
 
 generate_heatmap(num_lib_frames, num_frames, lib_contour, vid_contour)
 
-% output_vid = VideoWriter('AD_Cone1Animation.avi'); % Attidude Determination video for Cone1 Animation
-% open(output_vid);
-% output_vid_frames = {num_frames};
-% 
-% for i = 1:1:num_frames
-%    writeVideo(output_vid, im2frame(lib_frames{index(i)}));
-%    imshow(lib_frames{index(i)});
-%    figure()
-% end
+output_vid = VideoWriter('Reconstruced Video'); % Attidude Determination video for Cone1 Animation
+open(output_vid);
 
-% writeVideo(output_vid, output_vid_frames);
+for i = 1:num_frames
+   I = lib_frames{scores(i)};
+   writeVideo(output_vid, I);
+end
+
+close(output_vid);
 
 toc
