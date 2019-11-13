477656a28557041bf07b4f1cb06721d5d403c7de
Sierra MacLeod <sierra.m.macleod@gmail.com>
2019-11-12 22:55:54 -0700

    preprocess config

--- config.m
+++
@@ -1,8 +0,0 @@
-% Calc Contour Gauss
-FILTER_WIDTH = 3;
-
-% Library Generation
-LIB_SUBSET_SIZE = 2;
-
-% Video Source
-VIDEO_FILE = "media/Cone1Animation.mp4";
--- preprocess.m
+++ preprocess.m
@@ -4,12 +4,15 @@
 % attitude determination. Input libraries are imported/created and processed here.
 %
 %
+addpath(genpath('YAMLMatlab/'));
+config = ReadYaml('config.yaml');
+
 close all; % Close any opened images
 
 tic
 
-[num_lib_frames, lib_frames] = create_library_frames(VIDEO_FILE, LIB_SUBSET_SIZE); % Create library frames from input animation
-[lib_contour] = calc_contour_gauss(num_lib_frames, lib_frames, FILTER_WIDTH); % Get contour of library frames
-[num_frames, vid_frames] = create_input_frames(VIDEO_FILE); % Create input frames from input animation
+[num_lib_frames, lib_frames] = create_library_frames(config.VIDEO_FILE, LIB_SUBSET_SIZE); % Create library frames from input animation
+[lib_contour] = calc_contour_gauss(num_lib_frames, lib_frames, config.FILTER_WIDTH); % Get contour of library frames
+[num_frames, vid_frames] = create_input_frames(config.VIDEO_FILE); % Create input frames from input animation
 
 toc % Display code runtime
