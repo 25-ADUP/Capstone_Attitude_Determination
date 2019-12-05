bfefc046d022430e955401f8e2a930aba4f8de63
Sierra MacLeod <sierra.m.macleod@gmail.com>
2019-12-04 20:10:53 -0700

    more progress

---
+++ cellfunprogress.m
@@ -0,0 +1,15 @@
+function cell = cellfunprogress (message, varargin)
+    bar = waitbar(0, message);
+    count = 0;
+    max = sum(size(varargin{2}));
+    
+    cell = cellfun(@(item)innerfun(item), varargin{2:end});
+    
+    close(bar);
+
+    function result = innerfun(item)
+        result = varargin{1}(item);
+        count = count + 1;
+        waitbar(count / max, bar, message);
+    end
+end
--- get_video_frames.m
+++ get_video_frames.m
@@ -11,8 +11,11 @@ function [num_frames, vid_frames, frame_rate] = get_video_frames(vid_file)
     i = 1; % variable to iterate through vid_frames
 
     % Should try and vectorize this!
+    bar = waitbar(0, 'Fetching input video...');
     while hasFrame(vreader) % while MATLAB can find videoframes, put each frame into vid_frames
         vid_frames{i} = image_fit(readFrame(vreader)); % image_fit will downsample the image if neccesary
         i = i + 1;
+        waitbar(i / num_frames, bar, 'Fetching input video..');
     end
+    close(bar);
 end
