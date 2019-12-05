9dfeb2145b5a10a13fb62077376e4c466e95d5ac
toriroyal <tori.royal@sbcglobal.net>
2019-12-04 18:38:51 -0700

    Won't make same contours over and over again!

--- preprocess.m
+++ preprocess.m
@@ -17,18 +17,22 @@ db = ModelStorage(config.DATABASE_ZIP); % Open priors database
 
 lib_images = fetch_images(db, '', '', ''); % Fetch all library images
 
-lib_contours = cellfun(@(x) calc_contour_gauss(x.image, config.FILTER_WIDTH), lib_images, 'UniformOutput', false); % Get contour of library images
+if config.NEW_LIB == true
+    lib_contours = cellfun(@(x) calc_contour_gauss(x.image, config.FILTER_WIDTH), lib_images, 'UniformOutput', false); % Get contour of library images if they do not already exist
+else
+    lib_contours = fetch_contours(db, '', '', ''); % Fetch contours if they already exist
+end
 
 theta = cellfun(@(x) uint16(x.theta), lib_images); % Get the prior angle data for theta, psi, phi
 psi = cellfun(@(x) uint16(x.psi), lib_images);
 phi = cellfun(@(x) uint16(x.phi), lib_images);
 
-[rows, cols] = size(lib_images); % Get the dimensions of the cell arrays
+[num_lib_images, y] = size(lib_images); % Get the dimensions of the cell arrays
 
-for i = 1:1:rows
+for i = 1:1:num_lib_images
     db.insert_contour(theta(i), psi(i), phi(i), lib_contours{i}) % Insert contours into database
 end
 
-[num_frames, vid_frames] = create_input_frames(config.VIDEO_FILE, config.SIGMA); % Create input frames from input animation
+[num_frames, vid_frames, frame_rate] = create_input_frames(config.VIDEO_FILE, config.SIGMA); % Create input frames from input animation
 
 toc % Display code runtime
