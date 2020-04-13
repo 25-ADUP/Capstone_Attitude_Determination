050c239b65bffc7e9c13e4e3cde53667ba48249b
toriroyal <tori.royal@sbcglobal.net>
2020-04-13 15:58:17 -0600

    Added output video functionality

--- live.m
+++ live.m
@@ -17,7 +17,7 @@ estimated_attitudes(1) = first_score{1};
 for i = 2:1:num_frames % Use similars to narrow estimated attitude search & calculation
     next_score = compare(num2cell(previous_similars), vid_contours(i)); % Sending in similars as a cell array
     estimated_attitudes(i) = next_score{1};
-    [prec_contours, previous_similars] = library.get(lib_contours{next_score{1}}.theta, lib_contours{next_score{1}}.phi, lib_contours{next_score{1}}.psi);
+    [prec_contours, previous_similars] = library.get(lib_contours{next_score{1}}.theta, lib_contours{next_score{1}}.psi, lib_contours{next_score{1}}.phi);
 end
 
 toc
